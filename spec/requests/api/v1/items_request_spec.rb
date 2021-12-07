fca1995b08cf2c33324c494c6c2abcb322f2f261
Stephanie Helm <stephaniemhelm@gmail.com>
2021-12-06 19:46:53 -0700

    items controller

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -43,4 +43,80 @@ describe "Items API" do
       expect(item[:merchant_id]).to be_a(Integer)
     end
   end
+
+  it 'can get on item by its id' do
+    merchant = create(:merchant)
+    item1 = create(:item, merchant_id: merchant.id)
+
+    get "/api/v1/items/#{item1.id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to eq(item1.id)
+
+    expect(item).to have_key(:name)
+    expect(item[:name]).to be_a(String)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_a(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_a(Float)
+
+    expect(item).to have_key(:merchant_id)
+    expect(item[:merchant_id]).to be_a(Integer)
+  end
+
+  it 'can create a new item' do
+    merchant = create(:merchant)
+
+    item_params = ({
+                    name: 'Couch',
+                    description: 'Soft, warm and cozy',
+                    unit_price: 499.9,
+                    merchant_id: merchant.id
+                    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
+
+  it 'can update an existing item' do
+    merchant = create(:merchant)
+    id = create(:item, merchant_id: merchant.id).id
+    previous_name = Item.last.name
+    item_params = { name: 'Sofa'}
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq('Sofa')
+  end
+
+  it 'can destory an item' do
+    merchant = create(:merchant)
+    item1 = create(:item, merchant_id: merchant.id)
+
+    expect(Item.count).to eq(1)
+    delete "/api/v1/items/#{item1.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(item1.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
